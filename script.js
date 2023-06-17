81c5b5f2c864ade5fff933714c55d0ebda984f57
Michael <m.anestopoulos24@gmail.com>
2023-06-17 12:22:34 -0400

    made basic assing book to array function

---
+++ script.js
@@ -0,0 +1,46 @@
+let bookTitle = document.getElementById('title');
+let bookAuthor = document.getElementById('author');
+let bookPages = document.getElementById('pages');
+let bookRead = document.getElementById('read');
+
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return(title + " by " + author + ", " + pages + " pages, " + read)
+    }
+}
+
+function addBookToLibrary(){
+    let book;
+    if(bookRead.checked){
+        book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, 'read');
+    }
+    else{
+        book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, 'not read');
+    }
+    myLibrary.push(book);
+    console.log(myLibrary);
+    clearInputs();
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
+myLibrary.push(theHobbit);
+
+
+console.log(myLibrary);
+
+
+
+//Anonymous Functions
+function clearInputs() {
+    let inputs = document.querySelectorAll('input[type="text"]');
+    inputs.forEach(function(input) {
+      input.value = "";
+    });
+    bookRead.checked = false;
+  }
